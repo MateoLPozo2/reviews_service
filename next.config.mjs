7e1d6941c7d62919074876fea2c85771160b1e5e
Mateo Lopez <lopezpozomateo@gmail.com>
2025-06-16 13:40:13 +0200

    Rename next.config.js to next.config.mjs to fix ESM error

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+<<<<<<< HEAD
   reactStrictMode: true,
 };
 
 export default nextConfig;
+=======
+  experimental: {
+    serverComponentsExternalPackages: ['@neondatabase/serverless']
+  },
+  images: {
+    domains: ['localhost'],
+  },
+}
+
+module.exports = nextConfig
+>>>>>>> 53526ed (Rename next.config.js to next.config.mjs to fix ESM error)
