ac2f7eb386d8aba587b7184c6b35741d192a614d
Mateo Lopez <lopezpozomateo@gmail.com>
2025-06-16 13:52:59 +0200

    Fix experimental config and remove conflict with pages/index.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    serverComponentsExternalPackages: ['@neondatabase/serverless']
-  },
+experimental: {
+  serverExternalPackages: ['@neondatabase/serverless']
+},
   images: {
     domains: ['localhost'],
   },
