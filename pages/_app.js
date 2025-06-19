38891c03e12cbbaa32faf596e3e99c65356701c2
Mateo Lopez <lopezpozomateo@gmail.com>
2025-06-19 12:19:55 +0200

    Update imprint, and messages

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
 // pages/_app.js
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
-import { NextIntlProvider } from "next-intl";
 
 export default function App({ Component, pageProps }) {
   return (
-    <NextIntlProvider messages={pageProps.messages}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </NextIntlProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
