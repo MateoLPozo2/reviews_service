e29b9e40c7a17cae6e0bf718a50f76ef847e3068
Mateo Lopez <lopezpozomateo@gmail.com>
2025-07-24 14:59:28 +0200

    Refactored [slug].js

--- pages/mlp/reviews/[nid]/[slug].js
+++ pages/mlp/reviews/[nid]/[slug].js
@@ -2,7 +2,6 @@ import { useRef } from "react";
 import { getReviewById } from "@/services/reviewService";
 import SlugValidator from "@/interfaces/SlugValidator";
 import MarkdownViewer from "@/components/MarkdownViewer";
-import ExportButton from "@/components/ExportButton";
 import ReviewMeta from "@/components/ReviewMeta";
 
 export async function getServerSideProps({ params, res }) {
@@ -32,8 +31,8 @@ export default function ReviewPage({ review }) {
     <div className="p-6 space-y-4">
       <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-4">
         <h1 className="text-3xl font-bold">{review.title}</h1>
-        <div className="flex gap-2 flex-wrap" />
       </div>
+      <ReviewMeta review={review} contentRef={contentRef} />
       <div
         ref={contentRef}
         className="prose dark:prose-invert bg-white dark:bg-neutral-900 text-black dark:text-white p-4 rounded shadow max-w-none"
